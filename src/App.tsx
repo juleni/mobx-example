c952fe5468dde4c548db71743ba9737d4fb80afb
juleni <69912179+juleni@users.noreply.github.com>
2024-03-27 17:12:18 +0100

    Added TeamStoreProvider and TeamInfo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import "./App.css";
+import Athlete from "./Athlete";
 import MoneyForm from "./MoneyForm";
 import Roster from "./Roster";
+import TeamInfo from "./TeamInfo";
+import { TeamStoreProvider } from "./TeamStore";
+
+const lebronJames = new Athlete("Lebron James", 37, 9000);
+const stephCurry = new Athlete("Steph Curry", 34, 5000);
+
+function getPlayersFromBackend(): Athlete[] {
+  return [lebronJames, stephCurry];
+}
 
 function App() {
+  const players = getPlayersFromBackend();
   return (
     <div className="App">
       <header className="App-header">
-        <Roster />
-        <MoneyForm />
+        <TeamStoreProvider players={players}>
+          <TeamInfo />
+          <Roster />
+          <MoneyForm />
+        </TeamStoreProvider>
       </header>
     </div>
   );
